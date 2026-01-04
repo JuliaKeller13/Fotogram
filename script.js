5256bdb674b3ef1a7c8416df7c2defe73a7cb73a
JuliaKeller13 <juliakeller.vk@gmail.com>
2026-01-04 12:07:33 +0100

    projekt start added

---
+++ script.js
@@ -0,0 +1,38 @@
+const dialogRef = document.getElementById("dialogMode");
+
+const imgs = [
+  "./img/bird.jpg",
+  "./img/clouded.png",
+  "./img/duck.jpg",
+  "./img/erde.jpg",
+  "./img/fields.jpg",
+  "./img/fluffy-bird-string.jpg",
+   "./img/landscape.jpg",
+  "./img/leopard.jpg",
+  "./img/little-bird.jpg",
+  "./img/mountain-sky.jpg",
+  "./img/mountain.jpg",
+  "./img/night-city.jpg",
+  "./img/street.jpg",
+  "./img/tree.jpg",
+];
+
+function openDialog() {
+  dialogRef.showModal();
+}
+
+function closeDialog() {
+  dialogRef.close();
+}
+
+function generateImgs() {
+  const galerieRef = document.getElementById("galerie");
+
+  for (let i = 0; i < imgs.length; i++) {
+    galerieRef.innerHTML += getNotesHtml(i);
+  }
+}
+
+function getNotesHtml(i) {
+  return `<img class="img-klein-ansicht" src="${imgs[i]}" alt="Bild ${i + 1}">`;
+}
